40f4cacb85d8b7af0b1ea959dab6727cf6f04fb0
Ricardo Yamaguchi <ricardoyamaguchi2011@gmail.com>
2019-02-03 22:25:25 -0200

    Initial Commit

---
+++ marvel-gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marvel-gallery
+//
+//  Created by Ricardo Yamaguchi on 03/02/19.
+//  Copyright © 2019 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
