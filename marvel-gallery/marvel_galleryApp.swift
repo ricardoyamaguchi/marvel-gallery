9f53c2a572a67abb5bbf9f522fb49d6239c848b8
Ricardo Yamaguchi <ricardoyamaguchi2011@gmail.com>
2021-10-13 20:07:10 -0300

    First commit

---
+++ marvel-gallery/marvel_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_galleryApp.swift
+//  marvel-gallery
+//
+//  Created by Ricardo Yamaguchi on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct marvel_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ marvel-galleryUITests/marvel_galleryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  marvel_galleryUITestsLaunchTests.swift
+//  marvel-galleryUITests
+//
+//  Created by Ricardo Yamaguchi on 13/10/21.
+//
+
+import XCTest
+
+class marvel_galleryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
